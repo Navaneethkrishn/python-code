fe79032da853f653c068662b5f9babf9423bcb37
Navaneethkrishn <84831873+Navaneethkrishn@users.noreply.github.com>
2021-05-26 11:21:35 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+rad=float(input("ENTER THE RADIUS OF THE CIRCE"))
+PI=3.142
+AREA=PI*(rad*rad)
+print("THE AREA OF THE CIRCLE IS : ",AREA)
---
+++ fileextension.py
@@ -0,0 +1,3 @@
+Fname=input("ENTER THE FILE NAME : ")
+Fextension=Fname.split(".")
+print("THE EXTENSION OF THE FILE IS : " + repr(Fextension[-1]))
